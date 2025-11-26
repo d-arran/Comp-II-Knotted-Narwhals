ca72f717931361b7a6ef88a515b8eb11669d2d56
d-arran <nvxj38@durham.ac.uk>
2025-11-26 14:26:03 +0000

    Update Knot.m
    Included helper method to condense paths like [1,2;2,3] to [1,3] for later use in the Kauffman procedure

--- Knot.m
+++ Knot.m
@@ -3,12 +3,14 @@ classdef Knot
         diagram
         crossings
         writhe
+        kbracket
     end
     methods
         function obj = Knot(diagram)
             obj.diagram = diagram;
             obj.crossings = size(diagram,1);
             obj = obj.w();
+            obj = obj.kauffman();
         end
         function obj = w(obj)
             obj.writhe = 0;
@@ -18,6 +20,53 @@ classdef Knot
                     obj.writhe = obj.writhe+1;
                 else
                     obj.writhe = obj.writhe-1;
+                end
+            end
+        end
+        function obj = kauffman(obj)
+        end
+    end
+    methods (Static)
+        function path = optimise(path)
+            l = max(max(path));
+
+            [~,id] = sort(path(:,1));
+            path = path(id,:);
+            b = zeros(l,2);
+            b(:,1) = 1:l;
+
+            for i = 1:size(path,1)
+                if path(i,1) > path(i,2)
+                    path(i,:) = flip(path(i,:));
+                end
+            end
+
+            j = 1;
+            for i = path(:,1).'
+                b(i,2) = path(j,2);
+                j = j + 1;
+            end
+            
+            flags = zeros(1,l);
+            for i = 1:l
+                flags(i) = mod(sum(ismember(path,i),'all'),2);
+            end
+            
+            npaths = sum(flags)/2;
+            path = zeros(npaths,2);
+            
+            k = 1;
+            for i = 1:l
+                if flags(i) == 1
+                    t = b(i,2);
+                    path(k,1) = i;
+                    flags(i) = 0;
+                    while flags(t) == 0
+                        t = b(t,2);
+                    end
+                    path(k,2) = t;
+                    flags(t) = 0;
+                    k = k+1;
                 end
             end
         end
