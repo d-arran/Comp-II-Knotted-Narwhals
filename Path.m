acc8c1f880e25a4a818e7dfd0beb0a42ed210f8f
d-arran <nvxj38@durham.ac.uk>
2025-11-28 12:50:57 +0000

    Create Path.m
    Object class that stores the power of A and -(A^2+A^-2) = B as well as the simplest connected path on the resolved knot

---
+++ Path.m
@@ -0,0 +1,84 @@
+classdef Path
+    properties
+        coefficients
+        path
+    end
+    methods
+        function obj = Path(coefficients,path)
+            if nargin == 0
+                return;
+            end
+            obj.coefficients = coefficients;
+            obj.path = path;
+            obj = obj.optimise();
+        end
+        function obj = optimise(obj)
+            l = max(max(obj.path));
+            ref = zeros(l);
+            
+            for p = obj.path.'
+                ref(p(1),p(2)) = 1;
+                ref(p(2),p(1)) = 1;
+            end
+            
+            seeds = zeros(1,l);
+            for i = 1:l
+                seeds(i) = mod(sum(ismember(obj.path,i),'all'),2);
+            end
+            
+            % Finds all open paths
+            L = sum(seeds)/2;
+            newpaths = zeros(L,2);
+            seen = zeros(1,l);
+            j = 1;
+            for i = 1:l
+                t0 = 0;
+                t1 = i;
+                if seeds(t1) == 1
+                    seeds(t1) = 0;
+                    seen(t1) = 1;
+                    while seeds(t1) == 0
+                        t2 = find(ref(t1,:)==1);
+                        if size(t2) == 1 & t0 ~=0
+                        elseif ref(t1,t2(1)) == 1 && t2(1) ~= t0
+                            t0 = t1;
+                            t1 = t2(1);
+                            seen(t1) = 1;
+                        elseif ref(t1,t2(2)) == 1
+                            t0 = t1;
+                            t1 = t2(2);
+                            seen(t1) = 1;
+                        end
+                    end
+                    seeds(t1) = 0;
+                    newpaths(j,:) = [i,t1];
+                    j = j + 1;
+                end
+            end
+
+            obj.path = newpaths;
+            
+            % Finds all closed cycles
+            for i = 1:l
+                if seen(i) == 0
+                    t0 = 0;
+                    t1 = i;
+                    while seen(t1) == 0
+                        seen(t1) = 1;
+                        t2 = find(ref(t1,:) == 1);
+                        if size(t2) == 1
+                            seen(t2) = 1;
+                        elseif ref(t1,t2(1)) == 1 && t2(1) ~= t0
+                            t0 = t1;
+                            t1 = t2(1);
+                        else
+                            t0 = t1;
+                            t1 = t2(2);
+                        end
+                    end
+                    obj.coefficients(2) = obj.coefficients(2) + 1;
+                end
+            end
+        end
+    end
+end
