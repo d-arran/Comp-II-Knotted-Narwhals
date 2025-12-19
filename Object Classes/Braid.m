ec02faf3aef4528fe54f17dde3c8b8f591638e45
d-arran <nvxj38@durham.ac.uk>
2025-12-19 01:02:29 +0000

    Comments and last minute fixes

--- Object Classes/Braid.m
+++ Object Classes/Braid.m
@@ -11,16 +11,16 @@ classdef Braid
         function obj = Braid(word)
             obj.word = word;
             obj.strands = max(abs(obj.word))+1;
-            obj = obj.permutations();
-            obj = obj.components();
-            obj = obj.planar();
-            obj.closure = Knot(obj.diagram);
+            obj = obj.permutations(); % determine the positions of each strand after every permutation
+            obj = obj.components(); % determine the cycles present in the word
+            obj = obj.planar(); % create planar diagram
+            obj.closure = Knot(obj.diagram); % create knot object using previously calculated planar diagram
         end
         function obj = permutations(obj)
             length = size(obj.word,2);
             obj.p = zeros(length+1,obj.strands);
-            obj.p(1,:) = 1:obj.strands;
-            for i = 1:length
+            obj.p(1,:) = 1:obj.strands; % initialise permutation array
+            for i = 1:length % perform each permutation
                 t = abs(obj.word(i));
                 temp = obj.p(i,:);
                 temp(t:t+1) = flip(temp(t:t+1));
@@ -29,15 +29,15 @@ classdef Braid
         end
         function obj = components(obj)
             ref = [obj.p(end,:);obj.p(1,:)].';
-            [~,id] = sort(ref(:,1));
+            [~,id] = sort(ref(:,1)); % sort left column of reference array to line up 1-n without disturbing relations
             ref = ref(id,:);
-            seen = zeros(1,obj.strands);
-            obj.cycles = cell(0);
+            seen = zeros(1,obj.strands); % preallocate memory
+            obj.cycles = cell(0); % create empty cell array, no preallocation because we cant determine how many cycles there are
             for i = 1:obj.strands
-                if seen(i) == 0
+                if seen(i) == 0 % so we don't double count cycles
                     t = i;
                     cycle = zeros(0);
-                    while seen(t) == 0
+                    while seen(t) == 0 % go through reference array until looped back to original element
                         cycle(end+1) = t;
                         seen(t) = 1;
                         t = ref(t,2);
@@ -47,18 +47,18 @@ classdef Braid
             end
         end
         function obj = planar(obj)
-            obj.diagram = zeros(size(obj.word,2),4);
+            obj.diagram = zeros(size(obj.word,2),4); % preallocate memory
             i = 1;
-            for cycle = obj.cycles
+            for cycle = obj.cycles % loop over each component of the link
                 t = i;
-                for k = cycle{1}
-                    for j = 1:size(obj.word,2)
+                for k = cycle{1} % loop over each strand of the component
+                    for j = 1:size(obj.word,2) % loop over each transposition
                         w = obj.word(j);
                         pair = obj.p(j,abs(w):abs(w)+1);
-                        f = find(pair==k);
-                        id = f + sign(w);
-                        if size(f,2) == 0
-                        elseif id == 0
+                        f = find(pair==k); % determine the position of the current strand after the transposition
+                        id = f + sign(w); % each case is identified by this id, {1,2} x {-1,1} = {0,1,2,3}, saves one comparison
+                        if size(f,2) == 0 % pass if the strand is not affected by current transposition
+                        elseif id == 0 % label diagram accordingly
                             obj.diagram(j,1:2:3) = i:i+1;
                             i = i + 1;
                         elseif id == 1
@@ -73,7 +73,7 @@ classdef Braid
                         end
                     end
                 end
-                obj.diagram(obj.diagram==i) = t;
+                obj.diagram(obj.diagram==i) = t; % fix the last crossing
             end
         end
     end
--- Object Classes/Knot.m
+++ Object Classes/Knot.m
@@ -2,6 +2,8 @@ classdef Knot
     properties
         diagram
         crossings
+        components
+        linknumbers
         writhe
         kbracket
         jones
@@ -9,30 +11,95 @@ classdef Knot
     end
     methods
         function obj = Knot(diagram)
-            if nargin == 0 % edge case for empty knot
-                obj.crossings = 0;
-                obj.writhe = 0;
-                obj.kbracket = 0;
-                obj.jones = 0;
-                return;
-            end
             obj.diagram = diagram;
             obj.crossings = size(diagram,1);
+            obj = obj.lk(); % calculate the linking number
             obj = obj.w(); % calculate the writhe
             obj = obj.kauffman(); % calulcate the kauffman bracket and jones polynomial
         end
         function obj = w(obj)
             obj.writhe = 0;
-            L = max(max(obj.diagram));
             for i = 1:obj.crossings
-                if isequal([obj.diagram(i,2),obj.diagram(i,4)],[L,1])     % edge case
-                    obj.writhe = obj.writhe-1;
-                elseif isequal([obj.diagram(i,2),obj.diagram(i,4)],[1,L]) % edge case
-                    obj.writhe = obj.writhe+1;
-                elseif obj.diagram(i,2)<obj.diagram(i,4) % negative crossing
-                    obj.writhe = obj.writhe-1;
-                else                                     % positive crossing
-                    obj.writhe = obj.writhe+1;
+                if abs(obj.diagram(i,2)-obj.diagram(i,4)) == 1
+                    if obj.diagram(i,2)<obj.diagram(i,4) % negative crossing
+                        obj.writhe = obj.writhe - 1;
+                    else                                 % positive crossing
+                        obj.writhe = obj.writhe + 1;
+                    end
+                else                                     % edge cases
+                    for component = obj.components
+                        if ismember(obj.diagram(i,2),component{1})
+                            L = [component{1}(1), component{1}(end)];
+                        end
+                    end
+                    if isequal([obj.diagram(i,2),obj.diagram(i,4)],L) % final crossing in the link
+                        obj.writhe = obj.writhe + 1;
+                    else
+                        obj.writhe = obj.writhe - 1;
+                    end
+                end
+            end
+        end
+        function obj = lk(obj)
+            m = max(max(obj.diagram));
+            under = obj.diagram(:,1:2:3);
+            over = obj.diagram(:,2:2:4);
+            cross = [under; over];
+            ref = zeros(m); % preallocate memory
+            for c = cross.' % create adjacency matrix
+                ref(c(1),c(2)) = 1;
+                ref(c(2),c(1)) = 1;
+            end
+            obj.components = cell(0); % create empty cell array, no preallocation because we don't know how many components there are
+            i = 0;
+            while i ~= m
+                i = i + 1;
+                f = find(ref(i,:)==1); 
+                if size(f,2) == 0                 % edge case
+                    obj.components{end+1} = i;
+                elseif size(f,2) == 1             % edge case
+                    obj.components{end+1} = i:f;
+                    i = f;
+                else
+                    obj.components{end+1} = i:f(2); % this is the end of the cycle, we know that the arcs increment by 1 each time so we can use this as a shortcut
+                    i = f(2);
+                end
+            end
+            l = size(obj.components,2);
+            links = cell(0);
+            for i = 1:l % loop over each crossing to find which crossings are part of links
+                incross = [ismember(under(:,1),obj.components{i}),ismember(over(:,1),obj.components{i})];
+                t = mod(sum(ismember(incross,1),2),2).';
+                f = find(t==1);
+                links{end+1} = f; % store the rows for each link crossing (with the respective component number)
+            end
+            obj.linknumbers = zeros(l);
+            for i = 1:l-1
+                for j = i+1:l
+                    clinks = intersect(links{i},links{j}); % link crossings containing both components in question
+                    linknumber = 0;
+                    for link = clinks
+                        if abs(obj.diagram(link,2)-obj.diagram(link,4)) == 1
+                            if obj.diagram(link,2)<obj.diagram(link,4) % negative crossing
+                                linknumber = linknumber-1;
+                            else                                       % positive crossing
+                                linknumber = linknumber+1;
+                            end
+                        else                                           % edge cases
+                            for component = obj.components
+                                if ismember(obj.diagram(link,2),component{1})
+                                    L = [component{1}(1),component{1}(end)];
+                                end
+                            end
+                            if isequal([obj.diagram(link,2),obj.diagram(link,4)],L)
+                                linknumber = linknumber + 1;
+                            else
+                                linknumber = linknumber - 1;
+                            end
+                        end
+                    end
+                    obj.linknumbers(i,j) = linknumber;
+                    obj.linknumbers(j,i) = linknumber; % fill in the linking number matrix
                 end
             end
         end
